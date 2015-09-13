0cc6af6f3ff063ac5c3c57f4fe08d2f73e84a95f
unknown <Daniel_Martin@WN7X64-JQ0GVZ1.aus.amer.dell.com>
2015-09-12 23:02:09 -0500

    Added comments to code

--- plot1.R
+++ plot1.R
@@ -1,9 +1,13 @@
 plot1 <- function() {
+## Download and unzip file 
 fileUrl <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip'
 download.file(fileUrl, destfile ="./Household_Power_data")
+## Read into R
 data <-read.table(unzip("./Household_Power_data"), header = TRUE, sep = ";", na.strings = "?")
+##Convert Date column to date format and subset only the 2 desired dates
 data$Date <- as.Date(data$Date)
 HH_data <- subset(data,Date=="2007-02-01"|Date=="2007-02-02")
+## Call PNG function and then plot a histrogram before closing graphics device
 png("rplot1.png", width = 480, height = 480)
 hist(HH_data$Global_active_power, freq = TRUE, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,16 +1,24 @@
 plot3 <- function(){
   library(dplyr)
+  ## Download and unzip file 
   fileUrl <- 'https://archive.ics.uci.edu/ml/machine-learning-databases/00235/household_power_consumption.zip'
-  download.file(fileUrl, destfile ="./HouseholdPower")
-  data <-read.table(unzip("./HouseholdPower"), header = TRUE, sep = ";", na.strings = "?")
+  download.file(fileUrl, destfile ="./Household_Power_data")
+  ## Read into R
+  data <-read.table(unzip("./Household_Power_data"), header = TRUE, sep = ";", na.strings = "?")
+  ##Convert Date column to date format and subset only the 2 desired dates
   data$Date <- as.Date(data$Date)
   HH_data <- subset(data,Date=="2007-02-01"|Date=="2007-02-02")
+  ## Convert Time to character
   HH_data$Time <- as.character(HH_data$Time)
+  ##Merge date & time columns into one creating a new column
   HH_data <- mutate(HH_data,DateTime=as.POSIXct(paste(HH_data$Date, HH_data$Time), format="%Y-%m-%d %H:%M:%S"))
+  ## Call PNG function and then plot data before closing graphics device
   png("rplot3.png", width = 480, height = 480)
   plot(HH_data$DateTime,HH_data$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
+  ## Add 2nd and 3rd sub_metering lines
   lines(HH_data$DateTime,HH_data$Sub_metering_2, col = "red")
   lines(HH_data$DateTime,HH_data$Sub_metering_3, col = "blue")
+  ## Add legend
   legend("topright", col = c("black","red","blue"), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty = c(1,1))
   dev.off()
 }
